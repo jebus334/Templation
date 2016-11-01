9fad36da6648202a52819a284e8e21c8eb866d76
Dominik Skorynko <Techtips365yt@gmail.com>
2016-11-01 15:28:39 -0500

    Templation.IOS

---
+++ Templation/Templation.ios/Templation.ios/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Templation.ios
+//
+//  Created by mtech on 10/25/16.
+//  Copyright © 2016 Dominik. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
